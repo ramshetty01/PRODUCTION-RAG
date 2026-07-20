fcf4918b53f3892338604dd550599457ace9448b
Ramshetty02 <dyavarashetty.ram@gmail.com>
2026-07-20 08:09:40 +0530

    Add Qdrant runtime dependency

--- tests/test_optional_dependencies.py
+++ tests/test_optional_dependencies.py
@@ -2,3 +2,9 @@ def test_s3_storage_dependency_is_available():
     import boto3
 
     assert boto3.client
+
+
+def test_qdrant_vector_dependency_is_available():
+    from langchain_qdrant import QdrantVectorStore
+
+    assert QdrantVectorStore
